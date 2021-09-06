a70c886e69316d658f85e5d00b925e245c390080
James FE <jamesfrontend@gmail.com>
2021-09-06 16:06:19 -0400

    feat: vanilla js stuff

--- config/navItems.ts
+++ config/navItems.ts
@@ -1,5 +1,13 @@
 export const navItems = [
   {
+    label: "Sort Table (Vanilla JS)",
+    href: "https://jamesfrontend.github.io",
+  },
+  {
+    label: "Tic Tac Toe (Vanilla JS)",
+    href: "https://jamesfrontend.github.io/tictactoe",
+  },
+  {
     label: "API Calls",
     href: "/api-calls",
   },
